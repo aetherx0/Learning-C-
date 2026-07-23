657a3f5c5313475f3daec14de81c4417b3e9ae0f
aetherx0 <ameyaraut2708@gmail.com>
2026-07-24 03:20:04 +0530

    introduction

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    std::cout<<"Hello World!"<<std::endl;
+    std::cout<<"I M AMEYA!!!"<<std::endl;
+    std::cout<<"i m learning c++"<<std::endl;
+    return 0;
+}
